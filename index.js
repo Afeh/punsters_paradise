3a6c3cbd691ec0a47b58dfc11990871a5492efb7
Afebu Balogun <balogunafebu22@gmail.com>
2024-09-23 16:55:56 +0100

    Remove console.log from the code logic

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", async (req, res) =>  {
 	try {
 		const response = await axios.get(API_URL); //Using Axios to make a get request to the APi and storing the response
 		const result = response.data; //Storing the data part of the response 
-		console.log(result); // Checking to see the response from the request
+		// console.log(result); Checking to see the response from the request
 		res.render("index.ejs", {setup:result.setup, delivery:result.delivery}) // rendering the joke setup and punchline to the index.ejs file
 	} catch (error) {
 		console.error("Failed to make request", error.message);// Error Handling
